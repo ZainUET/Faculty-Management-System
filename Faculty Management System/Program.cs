22924e0606d3fd50970176d137ec3b5c1cf4e940
ZainUET <zain41uet@gmail.com>
2025-06-07 01:36:09 +0500

    Add files via upload

---
+++ Faculty Management System/Program.cs
@@ -0,0 +1,19 @@
+using DB_Mid_Project.UI;
+
+namespace DB_Mid_Project
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summarty>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Login());
+        }
+    }
+}
